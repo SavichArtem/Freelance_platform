dd0c049175a44dedaa1bec3b30731b34d30e2bd9
SavichArtem <mods_b@mail.ru>
2026-04-29 16:50:53 +0300

    feat: add PrivateRoute and AdminRoute components

---
+++ src/components/PrivateRoute/AdminRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const AdminRoute = ({ children }) => {
+  const { isAuthenticated, user, loading } = useSelector(state => state.auth);
+
+  if (loading) {
+    return <div className="loading">Загрузка...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (user?.role !== 'admin') {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default AdminRoute;
